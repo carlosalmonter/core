4291bcf6a959639d398b3fc4fb82ffbe1e02d512
Carlos Almonte <carlosalmonter@gmail.com>
2013-03-20 10:59:04 -0400

    creating rake file

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,5 +1,15 @@
-task :default => [:test]
 
-task :test do
-  ruby "test/unittest.rb"
+task :default => [
+    :update_repository ,
+    :run_migrations
+]
+
+task :update_repository do
+  cmd = "git pull"
+  sh cmd
+end
+
+task :run_migrations do
+  cmd = ".\vendor\bin\phinx migrate"
+  sh cmd
 end
